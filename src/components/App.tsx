d1a6969193d93cc118be09b903a2259b46396ebd
Otchy <id+github@otchy.net>
2022-07-13 00:39:32 +0900

    0.0.3: Introduce @otchy/sim-doc-db

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,5 @@
+import { Collection } from '@otchy/sim-doc-db';
+import { Field } from '@otchy/sim-doc-db/dist/types';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -6,6 +8,13 @@ import { Config } from './pages/Config';
 import { Home } from './pages/Home';
 
 export const App: React.FC = () => {
+  const fileds: Field[] = [{ name: 'key', type: 'tag', indexed: true }];
+  const collection = new Collection(fileds);
+  collection.add({ values: { key: 'aaa' } });
+  const result = collection.find({ key: 'aaa' });
+  console.log(Array.from(result));
+  console.log(collection.export());
+
   return (
     <HashRouter>
       <ul>
