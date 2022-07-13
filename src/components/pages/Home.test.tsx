75298b1599087a9617d1d009e0fe85200671f585
Otchy <id+github@otchy.net>
2022-07-14 01:51:44 +0900

    0.0.4: Update Home page description

--- src/components/pages/Home.test.tsx
+++ src/components/pages/Home.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Home } from './Home';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Home', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />);
+    const { asFragment } = render(<Home />, { wrapper: MemoryRouter });
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useOctokit } from '../../utils/useOctokit';
 
 export const Home: React.FC = () => {
@@ -11,5 +12,28 @@ export const Home: React.FC = () => {
       console.log(user);
     });
   }, []);
-  return <div>Home</div>;
+  return (
+    <>
+      <h2>GitHub Notes</h2>
+      <p>
+        GitHub Notes is a tool that open source version of Apple Notes or Google Keep which stores notes in your GitHub repository. So it is actually depending
+        on Microsoft but having much better control. All your notes are written as markdown documents.
+      </p>
+      <p>
+        You need to create a personal token via{' '}
+        <a href="https://github.com/settings/tokens/new" target="_blank">
+          this page
+        </a>{' '}
+        to use this tool. And the access token should have "repo" permission which provides full control of private repositories. So you may be worried if this
+        tool does something bad. In that case, you can always check the{' '}
+        <a href="https://github.com/otchy210/github-notes" target="_blank">
+          source code
+        </a>{' '}
+        of this tool and confirm there is no problem.
+      </p>
+      <p>
+        Did you create your access token? Set it up in the <Link to="/config">config</Link> page!
+      </p>
+    </>
+  );
 };
