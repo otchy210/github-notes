0a7d043d63a7804ca4fee731aa2b10bc24af9f0c
Otchy <id+github@otchy.net>
2022-08-01 23:39:44 +0900

    0.0.24: Save draft automatically

--- src/components/pages/Edit.tsx
+++ src/components/pages/Edit.tsx
@@ -1,12 +1,43 @@
 import React, { useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useURLSearchParams } from '../../hooks/useURLSearchParams';
+import { useLocalStorage } from '../../utils/useLocalStorage';
 
 export const Edit: React.FC = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const params = useURLSearchParams();
+  const key = params.get('key');
+  const localStorage = useLocalStorage();
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (key) {
+      const draft = localStorage.getDraft(key);
+      if (textareaRef.current) {
+        textareaRef.current.value = draft;
+      }
+      return;
+    }
+    const newKey = 'dummy-key';
+    navigate(`/edit?key=${newKey}`, { replace: true });
+  }, [key]);
 
   useEffect(() => {
     textareaRef.current?.focus();
-  }, []);
+    let prevDraft = textareaRef.current?.value;
+    const tid = window.setInterval(() => {
+      if (textareaRef.current && key) {
+        const draft = textareaRef.current.value;
+        if (draft !== prevDraft) {
+          localStorage.setDraft(key, draft);
+        }
+        prevDraft = draft;
+      }
+    }, 500);
+    return () => {
+      clearInterval(tid);
+    };
+  }, [key]);
 
   return (
     <>
--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -1,10 +1,13 @@
 const prefix = 'GitHubNotes-';
+const prefixDraft = `${prefix}Draft-`;
 
 export type LocalStorageNames = 'accessToken' | 'repository';
 
 type LocalStorage = {
   get: (name: LocalStorageNames, defaultValue?: string) => string;
   set: (name: LocalStorageNames, value: string) => LocalStorage;
+  getDraft: (key: string) => string;
+  setDraft: (key: string, draft: string) => LocalStorage;
 };
 
 const localStorage: LocalStorage = {
@@ -13,6 +16,13 @@ const localStorage: LocalStorage = {
   },
   set: (name: LocalStorageNames, value: string): LocalStorage => {
     window.localStorage.setItem(`${prefix}${name}`, value);
+    return localStorage;
+  },
+  getDraft: (key: string): string => {
+    return window.localStorage.getItem(`${prefixDraft}${key}`) ?? '';
+  },
+  setDraft: (key: string, draft: string) => {
+    window.localStorage.setItem(`${prefixDraft}${key}`, draft);
     return localStorage;
   },
 };
