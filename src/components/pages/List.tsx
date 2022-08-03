8480496d976308b0ea33c7a1a3c677c237f1d3ec
Otchy <id+github@otchy.net>
2022-08-03 23:16:22 +0900

    0.0.26: Add draft list

--- src/components/pages/List.tsx
+++ src/components/pages/List.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useLocalStorage } from '../../utils/useLocalStorage';
 import { NewNote } from '../common/NewNote';
 
 export const List: React.FC = () => {
+  const localStorage = useLocalStorage();
+  const drafts = localStorage.listDraft();
   return (
-    <div>
+    <div className="list">
+      <h2>Draft</h2>
+      <ul>
+        {drafts.map((draft) => {
+          return (
+            <li>
+              <Link to={`/edit?key=${draft.key}`}>
+                <h3>{draft.title}</h3>
+                <small>{draft.body}</small>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
       <NewNote />
     </div>
   );
--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -3,11 +3,18 @@ const prefixDraft = `${prefix}Draft-`;
 
 export type LocalStorageNames = 'accessToken' | 'repository';
 
+type Draft = {
+  key: string;
+  title: string;
+  body: string;
+};
+
 type LocalStorage = {
   get: (name: LocalStorageNames, defaultValue?: string) => string;
   set: (name: LocalStorageNames, value: string) => LocalStorage;
   getDraft: (key: string) => string;
   setDraft: (key: string, draft: string) => LocalStorage;
+  listDraft: () => Draft[];
 };
 
 const localStorage: LocalStorage = {
@@ -24,6 +31,21 @@ const localStorage: LocalStorage = {
   setDraft: (key: string, draft: string) => {
     window.localStorage.setItem(`${prefixDraft}${key}`, draft);
     return localStorage;
+  },
+  listDraft: (): Draft[] => {
+    return Object.entries(window.localStorage)
+      .filter(([key]) => {
+        return key.startsWith(prefixDraft);
+      })
+      .sort(([keyLeft], [keyRight]) => {
+        return keyLeft.localeCompare(keyRight) * -1;
+      })
+      .map(([key, draft]) => {
+        const lines = draft.split('\n');
+        const title = lines.length > 0 ? lines[0] : '<empty>';
+        const body = lines.length > 1 ? lines.slice(1).join(' ') : '<empty>';
+        return { key: key.slice(prefixDraft.length), title, body };
+      });
   },
 };
 
