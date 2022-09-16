2643d87bb69fc0ddbe651caa4a13250d4611d781
Otchy <id+github@otchy.net>
2022-09-16 22:56:50 +0900

    0.0.60: Highlight search query in the view

--- src/components/common/Render.tsx
+++ src/components/common/Render.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import { useSearchQuery } from '../../providers/SearchQueryProvider';
 import { useMarkdown } from '../../utils/useMarkDown';
 
 type Props = {
   text: string;
 };
 
+const highlight = (html: string, query: string) => {
+  const root = document.createElement('div');
+  root.innerHTML = html;
+  const addSpan = (elem: Node) => {
+    if (elem.nodeType === Node.TEXT_NODE) {
+      const textContent = elem.textContent as string;
+      if (textContent.trim().length === 0) {
+        return;
+      }
+      const parent = elem.parentElement;
+      if (!parent) {
+        return;
+      }
+      const highlightedContent = textContent.replaceAll(query, `<span class="highlight">${query}</span>`);
+      if (textContent === highlightedContent) {
+        return;
+      }
+      parent.innerHTML = highlightedContent;
+      return;
+    }
+    Array.from(elem.childNodes).forEach((child) => {
+      addSpan(child);
+    });
+  };
+  addSpan(root);
+  return root.innerHTML;
+};
+
 export const Render: React.FC<Props> = ({ text }) => {
   const md = useMarkdown();
   const renderedHTML = md.render(text);
+  const { query } = useSearchQuery();
+  const highlightedHTML = query ? highlight(renderedHTML, query) : renderedHTML;
 
-  return <div dangerouslySetInnerHTML={{ __html: renderedHTML }}></div>;
+  return <div dangerouslySetInnerHTML={{ __html: highlightedHTML }}></div>;
 };
